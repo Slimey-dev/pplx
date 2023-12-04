afb75b59a5a6c8b2f6d2ed345d1cbb28bb8fb3f3
Slimey-dev <djakub10g@gmail.com>
2023-12-04 14:06:51 +0100

    Add HTTP headers and payload to AI request

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use curl::easy::Easy;
+use curl::easy::{Easy, List};
 
 fn main() {
     tauri::Builder::default()
@@ -22,4 +22,22 @@ fn ai_request() {
     easy.url("https://api.perplexity.ai/chat/completions")
         .unwrap();
     easy.post(true).unwrap();
+
+    let mut list = List::new();
+    list.append("accept: application/json").unwrap();
+    list.append("content-type: application/json").unwrap();
+    easy.http_headers(list).unwrap();
+
+    let payload = r#"{
+        "model":"llama-2-70b-chat",
+        "messages":[{"role":"system","content":"string"}],
+        "max_tokens":0,
+        "temperature":1,
+        "top_p":1,
+        "top_k":0,
+        "stream":false,
+        "presence_penalty":0,
+        "frequency_penalty":1}"#;
+
+    easy.post_fields_copy(payload.as_bytes()).unwrap();
 }
