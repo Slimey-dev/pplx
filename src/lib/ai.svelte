0fa6e88bf1b5aad3b917f1c484d35ab81ab96c82
Slimey-dev <djakub10g@gmail.com>
2023-12-05 16:22:49 +0100

    Refactor AI response handling

--- src/lib/ai.svelte
+++ src/lib/ai.svelte
@@ -2,16 +2,19 @@
 	import { invoke } from '@tauri-apps/api/tauri';
 
 	let inputText = '';
-	let apiResponse: string = '';
+	let apiResponses: { message: string; isUser: boolean }[] = [];
 
 	async function callAiRequest(): Promise<void> {
+		apiResponses = [...apiResponses, { message: inputText, isUser: true }];
 		const result = await invoke('ai_request', { input: inputText });
-		apiResponse = result as string;
+		apiResponses = [...apiResponses, { message: result as string, isUser: false }];
 	}
 </script>
+
+{#each apiResponses as response (response.message)}
+	<pre class:is-user={response.isUser}>{response.message}</pre>
+{/each}
 
 <input bind:value={inputText} placeholder="Enter your input here" />
 
 <button on:click={callAiRequest}> Call AI Request </button>
-
-<pre>{apiResponse}</pre>
