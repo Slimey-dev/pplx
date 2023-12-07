90e353e1cefdd0dd3f8933a82e23f35bcc04d4cf
Slimey-dev <djakub10g@gmail.com>
2023-12-07 13:20:55 +0100

    Refactor main.rs file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,140 +10,141 @@ use tauri::Manager;
 use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem};
 
 fn main() {
-    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
 
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(hide)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(quit);
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(hide)
+    .add_native_item(SystemTrayMenuItem::Separator)
+    .add_item(quit);
 
-    let tray = SystemTray::new().with_menu(tray_menu);
+  let tray = SystemTray::new().with_menu(tray_menu);
 
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![async_command])
-        .system_tray(tray)
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
-                "quit" => {
-                    std::process::exit(0);
-                }
-                "hide" => {
-                    let window = app.get_window("main").unwrap();
-                    window.hide().unwrap();
-                }
-                _ => {}
-            },
-            _ => {}
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![async_command])
+    .system_tray(tray)
+    .on_system_tray_event(|app, event| match event {
+      SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+        "quit" => {
+          std::process::exit(0);
+        }
+        "hide" => {
+          let window = app.get_window("main").unwrap();
+          window.hide().unwrap();
+        }
+        _ => {}
+      },
+      _ => {}
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
 
 #[tauri::command]
 async fn async_command(input: &str) -> Result<String, String> {
-    let result = ai_request(&input).await;
-    result
+  let result = ai_request(&input).await;
+  result
 }
 
 lazy_static! {
-    static ref CHAT_HISTORY: Mutex<Vec<HashMap<&'static str, String>>> = Mutex::new(Vec::new());
+  static ref CHAT_HISTORY: Mutex<Vec<HashMap<&'static str, String>>> = Mutex::new(Vec::new());
 }
 
 async fn ai_request(input: &str) -> Result<String, String> {
-    let client = reqwest::Client::new();
+  let client = reqwest::Client::new();
 
-    {
-        let mut chat_history = CHAT_HISTORY.lock().unwrap();
-        if chat_history.is_empty() {
-            let mut system_msg_map: HashMap<&'static str, String> = HashMap::new();
-            system_msg_map.insert("role", "system".to_string());
-            system_msg_map.insert(
-                "content",
-                "Be precise and concise. Write your messages in markdown. Write codeblocks in markdown.".to_string(),
-            );
-            chat_history.push(system_msg_map);
-        }
-
-        let new_message = input.to_string();
-
-        let mut new_msg_map: HashMap<&'static str, String> = HashMap::new();
-        new_msg_map.insert("role", "user".to_string());
-        new_msg_map.insert("content", new_message);
-
-        chat_history.push(new_msg_map);
+  {
+    let mut chat_history = CHAT_HISTORY.lock().unwrap();
+    if chat_history.is_empty() {
+      let mut system_msg_map: HashMap<&'static str, String> = HashMap::new();
+      system_msg_map.insert("role", "system".to_string());
+      system_msg_map.insert(
+        "content",
+        "Be precise and concise. Write your messages in markdown. Write codeblocks in markdown."
+          .to_string(),
+      );
+      chat_history.push(system_msg_map);
     }
 
-    let chat_history_json = {
-        let chat_history = CHAT_HISTORY.lock().unwrap();
-        serde_json::to_string(&(*chat_history)).unwrap()
-    };
+    let new_message = input.to_string();
 
-    let messages: Vec<serde_json::Value> = serde_json::from_str(&chat_history_json).unwrap();
+    let mut new_msg_map: HashMap<&'static str, String> = HashMap::new();
+    new_msg_map.insert("role", "user".to_string());
+    new_msg_map.insert("content", new_message);
 
-    let payload = json!({
-        "model": "pplx-7b-chat",
-        "messages": messages,
-        "max_tokens": 0,
-        "temperature": 1,
-        "top_p": 1,
-        "top_k": 0,
-        "stream": false,
-        "presence_penalty": 0,
-        "frequency_penalty": 1
-    });
+    chat_history.push(new_msg_map);
+  }
 
-    println!("Payload: {:?}", payload);
+  let chat_history_json = {
+    let chat_history = CHAT_HISTORY.lock().unwrap();
+    serde_json::to_string(&(*chat_history)).unwrap()
+  };
 
-    println!("Sending request...");
+  let messages: Vec<serde_json::Value> = serde_json::from_str(&chat_history_json).unwrap();
 
-    let send_res = client
-        .post("https://api.perplexity.ai/chat/completions")
-        .header(ACCEPT, "application/json")
-        .header(CONTENT_TYPE, "application/json")
-        .header(
-            AUTHORIZATION,
-            "Bearer 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",
-        )
-        .body(payload.to_string())
-        .send()
-        .await;
+  let payload = json!({
+      "model": "pplx-7b-chat",
+      "messages": messages,
+      "max_tokens": 0,
+      "temperature": 1,
+      "top_p": 1,
+      "top_k": 0,
+      "stream": false,
+      "presence_penalty": 0,
+      "frequency_penalty": 1
+  });
 
-    println!("Request sent. Processing response...");
+  println!("Payload: {:?}", payload);
 
-    let res = match send_res {
-        Ok(val) => {
-            println!("Response received.");
-            val
-        }
-        Err(err) => {
-            println!("Error sending request: {}", err);
-            return Err(err.to_string());
-        }
-    };
+  println!("Sending request...");
 
-    let json_res: Result<serde_json::Value, _> = res.json().await;
-    let json = match json_res {
-        Ok(val) => {
-            println!("JSON response: {}", val.to_string());
-            val
-        }
-        Err(err) => return Err(err.to_string()),
-    };
+  let send_res = client
+    .post("https://api.perplexity.ai/chat/completions")
+    .header(ACCEPT, "application/json")
+    .header(CONTENT_TYPE, "application/json")
+    .header(
+      AUTHORIZATION,
+      "Bearer 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",
+    )
+    .body(payload.to_string())
+    .send()
+    .await;
 
-    let message = json["choices"][0]["message"]["content"]
-        .as_str()
-        .unwrap_or("");
+  println!("Request sent. Processing response...");
 
-    {
-        let mut chat_history = CHAT_HISTORY.lock().unwrap();
-
-        let mut assistant_msg_map: HashMap<&'static str, String> = HashMap::new();
-        assistant_msg_map.insert("role", "assistant".to_string());
-        assistant_msg_map.insert("content", message.to_string());
-
-        chat_history.push(assistant_msg_map);
+  let res = match send_res {
+    Ok(val) => {
+      println!("Response received.");
+      val
     }
+    Err(err) => {
+      println!("Error sending request: {}", err);
+      return Err(err.to_string());
+    }
+  };
 
-    Ok(message.to_string())
+  let json_res: Result<serde_json::Value, _> = res.json().await;
+  let json = match json_res {
+    Ok(val) => {
+      println!("JSON response: {}", val.to_string());
+      val
+    }
+    Err(err) => return Err(err.to_string()),
+  };
+
+  let message = json["choices"][0]["message"]["content"]
+    .as_str()
+    .unwrap_or("");
+
+  {
+    let mut chat_history = CHAT_HISTORY.lock().unwrap();
+
+    let mut assistant_msg_map: HashMap<&'static str, String> = HashMap::new();
+    assistant_msg_map.insert("role", "assistant".to_string());
+    assistant_msg_map.insert("content", message.to_string());
+
+    chat_history.push(assistant_msg_map);
+  }
+
+  Ok(message.to_string())
 }
